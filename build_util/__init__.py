7a7bc4af57b8bc2b94176a524d158cde99afa6b1
Kyle Treleaven <ktreleav@gmail.com>
2025-07-07 16:38:48 -0400

    adding notebook build target -- too bad terminal theme isn’t great for notebooks…

---
+++ build_util/__init__.py
@@ -0,0 +1,36 @@
+import subprocess as sp
+import pathlib
+
+DIR = pathlib.Path(__file__).parent
+PROJECT_DIR = DIR.parent
+BUILD_DIR = PROJECT_DIR / "build"
+DOCS_DIR = PROJECT_DIR / "docs"
+
+def singleton(factory):
+    return factory()
+
+
+def noxrun(session, args_, *args, **kwargs):
+    return sp.run([
+        "nox", "-r", "-s", session, "--", *args_
+    ], *args, **kwargs)
+
+
+@singleton
+class vis_notebook:
+    """A nice notebook.
+
+    https://nbconvert.readthedocs.io/en/latest/usage.html#
+
+    """
+    notebook_file = PROJECT_DIR / "demo" / "visualization.ipynb"
+
+    output_dir = DOCS_DIR / "notebooks"
+    output_file = output_dir / "visualization.md"
+
+    def create(self):
+        noxrun("nbenv", [
+            *"jupyter nbconvert --to markdown --execute".split(),
+            "--output", self.output_file,
+            self.notebook_file
+        ])
